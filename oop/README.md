c00cece5340cf306e9fbcaff89db9ba124e39142
Fredrik Svärd <fredrik.svard@gmail.com>
2018-01-30 22:47:10 +0100

    this works

---
+++ oop/README.md
@@ -0,0 +1,8 @@
+
+- Read a JSON-file, make a decision
+
+- Show what module you want to test
+
+- If tests succeed, install version in developtest envs
+
+- Send mail if fails or works
---
+++ oop/unittest.groovy
@@ -0,0 +1,45 @@
+/*
+class Example {
+   static void main(String[] args) {
+      Student mst = new Student();
+      mst.name = "Joe";
+      mst.ID = 1;
+      println(mst.Display())
+   } 
+}
+*/
+
+
+public class Student {
+   String name;
+   int ID;
+	
+   String Display() {
+      return name +ID;
+   }  
+}
+
+
+class StudentTest extends GroovyTestCase {
+   void testDisplay() {
+      def stud = new Student(name : 'Joe', ID : 1)
+      def expected = 'Joe1'
+      assertToString(stud.Display(), expected)
+      println 'hurra'
+   }
+	}
+
+import groovy.util.GroovyTestSuite 
+import junit.framework.Test 
+import junit.textui.TestRunner 
+
+class AllTests { 
+   static Test suite() { 
+      def allTests = new GroovyTestSuite() 
+      allTests.addTestSuite(StudentTest.class) 
+      //allTests.addTestSuite(EmployeeTest.class) 
+      return allTests 
+   } 
+} 
+
+TestRunner.run(AllTests.suite())
